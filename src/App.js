e512442f2ea48902d31d36ecf62bfc568a454ed1
Timothy Langham <graniteblood@gmail.com>
2021-03-29 18:34:37 -0400

    home pagee rendering, login page begun

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import axios from
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
-
+        <div className = "NavBar">
+        <h2> (NavBar) Logo + "Use My tech Stuff!  +  Link - Link - Link)</h2>
+        </div>
       </header>
+
+      <body>
+        <div className="main">
+          <h2>Products</h2>
+        </div>
+      </body>
+
+      <footer>
+        <div>
+          <h2>(NavBar) Contact Info - Link Link Link)</h2>
+        </div>
+      </footer>
+    </div>
   );
 }
 
---
+++ src/Components/Login.js
@@ -0,0 +1,23 @@
+import {React, useState} from 'react'
+
+
+
+
+// Initializations
+// States
+
+const [login, setLogin] = useState(login)
+// Return:
+
+export function Login {
+    <div className="login>"
+
+    return
+
+}
+<h2>Please Log-In<h2>
+ - username - textbox - 
+- password - textbox - VALIDATE
+<button>Sign Up!<button>
+
+
