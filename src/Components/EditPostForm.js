72715fef12fa89af0aa2776ff3c683b7a67dd264
Thomas Capello <thomcapello@gmail.com>
2021-03-30 10:42:54 -0400

    added edit post form

---
+++ src/Components/EditPostForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function EditPostForm(props) {
+
+    const {values, errors, change} = props;
+
+return (
+
+    <form>
+
+    <h3>Edit your Post</h3>
+
+    <div className='errors'>
+        <div>{errors}</div>
+    </div>
+
+    <label>Item:
+        <input name='item-name' type='text' value={values} onChange={change} />
+    </label>
+
+    <label>Item Description:
+        <input name='description' type='text' value={values} onChange={change} />
+    </label>
+
+    <label>Price:
+        <input name='price' type='text' value={values} onChange={change} />
+    </label>
+
+    <label>Rent Duration:
+        <select onChange={change} value={values} name="duration">
+            <option value="">- Select an option -</option>
+            <option value={"1 week"}>1 week</option>
+            <option value={"2 weeks"}>2 weeks</option>
+            <option value={"3 weeks"}>3 weeks</option>
+            <option value={"4 weeks"}>4 weeks</option>
+        </select>
+    </label>
+
+    <button>Edit Post</button>
+</form >
+)
+
+}
+
+
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CreatePostForm from './CreatePostForm';
+import EditPostForm from './EditPostForm';
 
 export default function HomePage() {
     return (
     <div>
       <h2>Home Page</h2>
       <CreatePostForm/>
+      <EditPostForm/>
     </div>)
   }
