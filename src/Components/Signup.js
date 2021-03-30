1d72ddd5b13d70a51dd54482730df3748ed2e6e6
Timothy Langham <graniteblood@gmail.com>
2021-03-30 01:21:45 -0400

    Rendering Version

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,146 +1,93 @@
-import React, {useEffect, useState} from 'react'
+import {React, useEffect} from 'react'
 import formSchema from '../validation/formSchema'
-import * as yup from "yup";
-
-// intial states
-const initialSUvalues = {
-
-	name: '',
-	username: '', //textbox
-	email: '', //textbox
-	password: '', //textbox
-	role: false, // checkbox
-	terms: false, // checkbox
-};
-
-const initialSUformErrors = {
-    name: '',
-	username: '',
-	email: '',
-	password: '',
-	role: "",
-	terms: "",
-  };
-
-const initialDisabled = true;
 
 
 
-const SignUp = () => {
 
-    const [suFormValues, setSUformValues] = useState(initialSUvalues); // object
-    const [formSUerrors, setSUerrors] = useState(initialSUformErrors); // object
-    const [disabled, setDisabled] = useState(initialDisabled); // boolean
+export default function SignUp(props) {
+    const { values, submit, change, disabled, errors } = props;
 
+    const onSubmit = (event) => {
+        event.preventDefault(); // stops page refresh, 
+        submit(); // invokes the submit function
+    };
 
-    // adjusting the status of disabled everytime values change
-    useEffect(() => {
-        formSchema
-            .isValid(suFormValues)
-            .then(valid => setDisabled(!valid))
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [suFormValues])
+    const onChange = (event) => {
+        const { name, value, type, checked } = event.target; 
+        const valueToUse = type === "checkbox" ? checked : value; 
+        change(name, valueToUse);
+    };
 
-
-//     // handles the validation
-//     yup
-//     .reach(formSchema, name) // get to this part of the schema
-//     //we can then run validate using the value
-//     .validate(value) // validate this value
-//     .then(() => {
-//       // happy path and clear the error
-//       setSUerrors({
-//         ...formSUerrors,
-//         [name]: "",
-//       });
-//     })
-//     // if the validation is unsuccessful, we can set the error message to the message
-//     // returned from yup (that we created in our schema)
-//     .catch((err) => {
-//         setSUerrors({
-//         ...formSUerrors,
-//         // validation error from schema
-//         [name]: err.errors[0],
-//       });
-//     });
-//     setSUformValues({
-//     ...suFormValues,
-//     [name]: value, // NOT AN ARRAY
-//   });
-
-const onSubmit = (evt) => {
-    evt.preventDefault();
-  };
 
     return (
-        <div className='sign-up-form'>
-           <form onSubmit={onSubmit}>
+        <form className='sign-up-form' onSubmit={onSubmit}>>
             <h3>Sign up for an Account</h3>
 
             <div className='errors'>
-                <div>{formSUerrors.name}</div>
-                <div>{formSUerrors.username}</div>
-                <div>{formSUerrors.email}</div>
-                <div>{formSUerrors.password}</div>
+                <div>{errors.username}</div>
+                <div>{errors.email}</div>
+                <div>{errors.password}</div>
             </div>
 
             <div className='inputs'>
-                <label>Name:
-                    <input
-                        name='name'
-                        type='text'
-                        value={suFormValues.name}
-                        placeholder='Please enter your full name'
-                    />
-                </label>
                 <label>Username:
                     <input
                         name='username'
                         type='text'
-                        value={suFormValues.username}
-                        placeholder='Please enter your username'
+                        placeholder='Choose a Username'
+                        maxLength="30"
+                        value={values.username}
+                        onChange={onChange}
                     />
                 </label>
+                <br/>
                 <label>Email:
                     <input
                         name='email'
                         type='text'
-                        value={suFormValues.email}
+                        maxLength="50"
+                        value={values.email}
+                        onChange={onChange}
                     />
                 </label>
+                <br/>
                 <label>Password:
                     <input
                         type='password'
                         name='password'
-                        value={suFormValues.password}
+                        maxLength="30"
+                        value={values.password}
+                        onChange={onChange}
                     />
                 </label>
+                <br/>
                 <label> Owner
                 <input
                     type="radio"
                     name="owner"
                     value="owner"
-                    checked={suFormValues.role === "owner"}
+                    checked={values.role === "owner"}
                 />
                 </label>
+                    <br/>
                 <label> Renter
                 <input
                     type="radio"
                     name="renter"
                     value="renter"
-                    checked={suFormValues.role === "renter"}
+                    checked={values.role === "renter"}
                 />
                 </label>
+                <br/>
                 <label>Terms and Conditions:
                     <input
                         type='checkbox'
                         name='terms'
-                        value={suFormValues.terms}
+                        checked={values.terms}
                     />
                 </label>
             </div>
             <button disabled={disabled}>Submit</button>
         </form>
-        </div> )
-        }
-export default SignUp;
+    )
+}
