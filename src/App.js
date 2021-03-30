a95c0d1ce0d519f5d8f52bf2033a38cd456ec4da
Tommy Capello <69427013+tommycapello@users.noreply.github.com>
2021-03-29 20:59:47 -0400

    fixed errors (#5)

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
 import React from "react";
-import { Route, NavLink } from "react-router-dom";
-import Login from "./Components/Login";
-import HomePage from './Components/HomePage'
+// import { Route, NavLink } from "react-router-dom";
+// import Login from "./Components/Login";
+// import HomePage from './Components/HomePage'
 import SignUp from './Components/SignUp'
 import "./App.css";
 
 
-const initialLoginValues = {
-  // Text Inputs
-  username: '',
-  password: '', 
-};
-
-const initialSignUpValues = {
-  username: '', //textbox
-  email: '', //textbox
-  password: '', //textbox
-  role: false, // checkbox
-  terms: false, // checkbox
-};
-
-const initialFormErrors = {
-  name: '',  // being blank, it's an error
-  password: '', // being blank, it's an 
-}
-
-const initialForm = []; // starting form is an EMPTY ARRAY, each form is an OBJECT
-const initialDisabled = true;
 
 export default function App() {
-  // STATES //
-const [login, setLogin] = useState(initialLoginValues);
 
-
-
-function App() {
 	return (
 		<div className="App">
-			<nav>
-			<NavLink className ='NavLink' to ='/'>
-				Home
-			</NavLink>
-			<NavLink className ='NavLink' to ='/login'>
-			Login
-			</NavLink>
-			<NavLink className ='NavLink' to ='/signup'>
-			Signup
-			</NavLink>
-			</nav>
-			<Route exact path="/" component={HomePage} />
-			<Route exact path='/login' component={Login} />
-			<Route exact path='/signup' component={SignUp} />
+			<SignUp/>
 		</div>
 	);
 }
-
-export default App;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,49 +1,87 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import formSchema from '../validation/formSchema'
 import * as yup from "yup";
 
-const SignUp = (props) => {
-    const {values, errors, disabled} = props
+// intial states
+const initialSUvalues = {
 
+	name: '',
+	username: '', //textbox
+	email: '', //textbox
+	password: '', //textbox
+	role: false, // checkbox
+	terms: false, // checkbox
+};
+
+const initialSUformErrors = {
+    name: '',
+	username: '',
+	email: '',
+	password: '',
+	role: "",
+	terms: "",
+  };
+
+const initialDisabled = true;
+
+
+
+const SignUp = () => {
+
+    const [suFormValues, setSUformValues] = useState(initialSUvalues); // object
+    const [formSUerrors, setSUerrors] = useState(initialSUformErrors); // object
+    const [disabled, setDisabled] = useState(initialDisabled); // boolean
+
+
+    // adjusting the status of disabled everytime values change
     useEffect(() => {
         formSchema
-            .isValid(values)
-            .then(valid => props.setDisabled(!valid))
+            .isValid(suFormValues)
+            .then(valid => setDisabled(!valid))
             // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [values])
+    }, [suFormValues])
 
-    useEffect(() => {
-        props.clearForm();
-         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
 
-    const handleChanges = (event) => {
-        const {value, name} = event.target
-        yup
-        .reach(formSchema,name)
-        .validate(value)
-        .then(() => {
-            props.setErrors(name ,'')
-        })
-        .catch(err => {
-            props.setErrors(name, err.errors[0])
-        })
-        props.setValues(value,name)
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.submit()
-    }
+//     // handles the validation
+//     yup
+//     .reach(formSchema, name) // get to this part of the schema
+//     //we can then run validate using the value
+//     .validate(value) // validate this value
+//     .then(() => {
+//       // happy path and clear the error
+//       setSUerrors({
+//         ...formSUerrors,
+//         [name]: "",
+//       });
+//     })
+//     // if the validation is unsuccessful, we can set the error message to the message
+//     // returned from yup (that we created in our schema)
+//     .catch((err) => {
+//         setSUerrors({
+//         ...formSUerrors,
+//         // validation error from schema
+//         [name]: err.errors[0],
+//       });
+//     });
+//     setSUformValues({
+//     ...suFormValues,
+//     [name]: value, // NOT AN ARRAY
+//   });
+
+const onSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
     return (
         <div className='sign-up-form'>
-           <form onSubmit={handleSubmit}>
+           <form onSubmit={onSubmit}>
             <h3>Sign up for an Account</h3>
 
             <div className='errors'>
-                <div>{errors.name}</div>
-                <div>{errors.username}</div>
-                <div>{errors.email}</div>
-                <div>{errors.password}</div>
+                <div>{formSUerrors.name}</div>
+                <div>{formSUerrors.username}</div>
+                <div>{formSUerrors.email}</div>
+                <div>{formSUerrors.password}</div>
             </div>
 
             <div className='inputs'>
@@ -51,8 +89,7 @@ const SignUp = (props) => {
                     <input
                         name='name'
                         type='text'
-                        onChange={handleChanges}
-                        value={values.username}
+                        value={suFormValues.name}
                         placeholder='Please enter your full name'
                     />
                 </label>
@@ -60,8 +97,7 @@ const SignUp = (props) => {
                     <input
                         name='username'
                         type='text'
-                        onChange={handleChanges}
-                        value={values.username}
+                        value={suFormValues.username}
                         placeholder='Please enter your username'
                     />
                 </label>
@@ -69,30 +105,42 @@ const SignUp = (props) => {
                     <input
                         name='email'
                         type='text'
-                        onChange={handleChanges}
-                        value={values.email}
+                        value={suFormValues.email}
                     />
                 </label>
                 <label>Password:
                     <input
                         type='password'
                         name='password'
-                        onChange={handleChanges}
-                        value={values.password}
+                        value={suFormValues.password}
                     />
+                </label>
+                <label> Owner
+                <input
+                    type="radio"
+                    name="owner"
+                    value="owner"
+                    checked={suFormValues.role === "owner"}
+                />
+                </label>
+                <label> Renter
+                <input
+                    type="radio"
+                    name="renter"
+                    value="renter"
+                    checked={suFormValues.role === "renter"}
+                />
                 </label>
                 <label>Terms and Conditions:
                     <input
                         type='checkbox'
-                        name='password'
-                        onChange={handleChanges}
-                        value={values.terms}
+                        name='terms'
+                        value={suFormValues.terms}
                     />
                 </label>
             </div>
             <button disabled={disabled}>Submit</button>
         </form>
-        </div>
-    )
-}
+        </div> )
+        }
 export default SignUp;
