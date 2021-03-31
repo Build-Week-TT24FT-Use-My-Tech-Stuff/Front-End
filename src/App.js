78756a3584a8c5d2a372b73036af922d75b3047b
WeiWu2 <weidiwu207@gmail.com>
2021-03-31 13:56:06 -0400

    fixed signup

--- src/App.js
+++ src/App.js
@@ -9,23 +9,7 @@ import formSchema from './validation/formSchema'
 import * as yup from "yup";
 
 // INITIALIZATIONS //
-const initialSUvalues = {
-	username: '', //textbox
-	email: '', //textbox
-	password: '', //textbox
-	role: false, // radio button
-	terms: false, // checkbox
-};
 
-const initialSUformErrors = {
-	username: '',
-	email: '',
-	password: '',
-	role: false,
-	terms: false,
-  };
-
-const initialDisabled = true;
 
 const initialForm = [];
 
@@ -34,67 +18,38 @@ export default function App() {
 
 	// STATES //
 const [form, setForm] = useState(initialForm);
-const [suFormValues, setSUformValues] = useState(initialSUvalues); // this is an object
-const [formSUerrors, setSUerrors] = useState(initialSUformErrors); // this is an object
-const [disabled, setDisabled] = useState(initialDisabled); // this is a boolean
+
 
 
 //  AXIOS POST //
-  const postNewUser = (newUser) => {
-    axios
-      .post("http://localhost:3000/", newUser)
-      .then((response) => {
-        setSUformValues([response.data, ...suFormValues]);
-        setSUformValues(initialSUvalues);
-        setDisabled(true);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const postNewUser = (newUser) => {
+  //   axios
+  //     .post("http://localhost:3000/", newUser)
+  //     .then((response) => {
+  //       setSUformValues([response.data, ...suFormValues]);
+  //       setSUformValues(initialSUvalues);
+  //       setDisabled(true);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
 
   // EVENT HANDLERS //
-  const inputChange = (name, value) => {
-    yup
-      .reach(formSchema, name) // search the schema for 'name'
-      .validate(value) // validate the entry (formValidation)
-      .then(() => {
-        setSUerrors({
-          ...formSUerrors,
-          [name]: "",
-        });
-      })
-      .catch((error) => {
-        setSUerrors({
-          ...formSUerrors,
-          // validation error from schema
-          [name]: error.errors,
-        });
-      });
-    setSUformValues({
-      ...suFormValues,
-      [name]: value,
-    });
-  };
+  
 
-const formSubmit = () => {
-    const newUser = {
-      username: suFormValues.username.trim(),
-      email: suFormValues.email.trim(),
-      password: suFormValues.password.trim(),
-      terms: suFormValues.termsOfService,
-    };
-    postNewUser(newUser);
-  };
+// const formSubmit = () => {
+//     const newUser = {
+//       username: suFormValues.username.trim(),
+//       email: suFormValues.email.trim(),
+//       password: suFormValues.password.trim(),
+//       terms: suFormValues.termsOfService,
+//     };
+//     postNewUser(newUser);
+//   };
 
-  useEffect(() => {
-    formSchema.isValid(suFormValues)
-      .then(valid => {
-        setDisabled(!valid)
-      })
-  }, [suFormValues])
-
+ 
 return (
     <Router>
       <div className="App">
@@ -113,15 +68,7 @@ return (
           <HomePage/>
         </Route>
         <Route path="/signup">
-            <SignUp values={suFormValues} submit={formSubmit} change={inputChange} disabled={disabled} errors={formSUerrors} />
-            {form.map((user, idx) => {
-              return (
-                <div key={idx}>
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-                </div>
-              )
-            })}
+            <SignUp/>
           </Route>
         <Route path="/login">
           <Login/>
