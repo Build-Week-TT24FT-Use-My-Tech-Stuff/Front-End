f3c73fca5e98235e871a335916cbd3adcff7072b
Tommy Capello <69427013+tommycapello@users.noreply.github.com>
2021-03-31 15:30:20 -0400

    Tc2 (#24)
    * this is a test
    
    * test 2
    
    * checking something out
    
    * wont change
    
    * login validation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import HomePage from './Components/HomePage'
 import SignUp from './Components/Signup'
 import "./App.css";
-import axios from 'axios'
-import formSchema from './validation/formSchema'
-import * as yup from "yup";
-
-// INITIALIZATIONS //
-
-
-const initialForm = [];
-
 
 export default function App() {
 
-	// STATES //
-const [form, setForm] = useState(initialForm);
-
-
-
-//  AXIOS POST //
-  // const postNewUser = (newUser) => {
-  //   axios
-  //     .post("http://localhost:3000/", newUser)
-  //     .then((response) => {
-  //       setSUformValues([response.data, ...suFormValues]);
-  //       setSUformValues(initialSUvalues);
-  //       setDisabled(true);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-
-
-  // EVENT HANDLERS //
-  
-
-// const formSubmit = () => {
-//     const newUser = {
-//       username: suFormValues.username.trim(),
-//       email: suFormValues.email.trim(),
-//       password: suFormValues.password.trim(),
-//       terms: suFormValues.termsOfService,
-//     };
-//     postNewUser(newUser);
-//   };
-
- 
 return (
     <Router>
       <div className="App">
