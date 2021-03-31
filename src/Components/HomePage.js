c7f48a6a68b264815d2be177875bdc7f7a793dd5
Ian Reed <opa1164@yahoo.com>
2021-03-31 14:42:51 -0500

    updated homePage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import CreatePostForm from './CreatePostForm';
-import EditPostForm from './EditPostForm';
+import Item from './Item';
+import axios from 'axios';
 
 export default function HomePage() {
+
+  const [items, setItems] = useState([]);
+  const [activeId, setActiveId] = useState(-1);
+
+  useEffect(() => {
+    axios.get('https://tt-24-use-my-tech-stuff.herokuapp.com/api/items')
+    .then(res => {
+      console.log(res.data);
+      setItems(res.data);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }, [])
+
+
+
     return (
     <div>
       <h2>Home Page</h2>
       <CreatePostForm/>
-      <EditPostForm/>
+      {items.length != 0 && items.map(item => <Item key = {item.item_id} item = {item} activeId = {activeId} setActiveId = {setActiveId}/>)}
+      <br/>
     </div>)
   }
