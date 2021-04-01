7071c5486be2b20cb3a78397bc3d69e981175195
Ian Reed <opa1164@yahoo.com>
2021-04-01 17:48:34 -0500

    added edit & delete feature

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,21 +1,44 @@
+import React from 'react'
+import EditPostForm from './EditPostForm';
+import axiosWithAuth from '../helpers/axiosWithAuth.js';
+
 export default function Item(props) {
-    return (
-      <li>
-        <div>
-            {props.name}
-        </div>
-        <div>
-            {props.description}
-            {props.price}
-        </div>
-        <div className="btn-group">
-          <button type="button">
-            Edit {props.name}
-          </button>
-          <button type="button" >
-            Delete {props.name}
-          </button>
-        </div>
-      </li>
-    );
+  const {item, activeId, setActiveId} = props;
+  
+
+  const handleClickEdit = () => {
+      setActiveId(item.item_id);
   }
+  const handleClickDelete = e => {
+    e.preventDefault();
+  axiosWithAuth().delete(`/items/item/${item.item_id}`)
+  .then(res => {
+    console.log(res);
+          setActiveId(-1);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  }
+
+  return (
+    <li>
+    <div>
+      <div>
+          {item.item_name}
+      </div>
+      <div>
+          {item.item_description}  ${item.item_price}
+      </div>
+      {activeId == item.item_id && <EditPostForm values = {item} setActiveId = {setActiveId}/>}
+      {activeId != item.item_id &&  <div className="btn-group">
+        <button type="button" onClick={handleClickEdit}>
+          Edit {props.name}
+        </button>
+        <button type="button" onClick={handleClickDelete}>
+          Delete {props.name}
+        </button>
+      </div>}
+    </div>
+    </li>
+    )}
